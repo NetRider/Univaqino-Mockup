7bde9e3904a49a8901a42fc7b52e8d91fd645b7b
Matteo Polsinelli <netrider91@gmail.com>
2016-01-12 16:47:30 +0100

    aggiunti filtri

---
+++ js/navigation.js
@@ -0,0 +1,39 @@
+$( document ).ready(function() {
+
+	$("#aulebar").on("click",function(){
+		impostaAule();
+	});
+
+	$("#followerbar").on("click",function(){
+		impostaFollowers();
+	});
+
+	impostaAule();
+});
+
+function impostaAule()
+{
+	$("#P0Coppito1").load("./AuleCoppito1.html #P0");
+	$("#P1Coppito1").load("./AuleCoppito1.html #P1");
+	$("#P2Coppito1").load("./AuleCoppito1.html #P2");
+	$(".active").removeClass("active");
+	$("#aulebar").addClass("active");
+}
+
+function impostaFollowers()
+{
+	$("#P0Coppito1").load("./FollowersCoppito1.html #P0");
+	$("#P1Coppito1").load("./FollowersCoppito1.html #P1");
+	$("#P2Coppito1").load("./FollowersCoppito1.html #P2");
+	$(".active").removeClass("active");
+	$("#followerbar").addClass("active");
+}
+
+function impostaProfessori()
+{
+	$("#P0Coppito1").load("./ProfessoriCoppito1.html #P0");
+	$("#P1Coppito1").load("./ProfessoriCoppito1.html #P1");
+	$("#P2Coppito1").load("./ProfessoriCoppito1.html #P2");
+	$(".active").removeClass("active");
+	$("#followerbar").addClass("active");
+}
