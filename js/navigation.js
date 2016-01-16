e9af65ac431bb3447949b6da246d796c4936e92d
Matteo Polsinelli <netrider91@gmail.com>
2016-01-16 13:35:52 +0100

    avvista prof bug fixed

--- js/navigation.js
+++ js/navigation.js
@@ -1,4 +1,4 @@
-$( document ).ready(function() {
+$(function() {
 
 	$("#freccia_indietro").css("visibility","hidden");
 
---
+++ js/profWatch.js
@@ -0,0 +1,21 @@
+$( document ).ready(function() {
+	console.log("profWathc.js");
+	$("#togglePosition").on("toggle",function(){
+		toggleStatus();
+	});
+
+});
+
+function toggleStatus()
+{
+	if($("#togglePosition").hasClass("active"))
+	{
+		$("#formGps").show();
+		$("#formNormal").hide();
+
+
+	}else {
+		$("#formGps").hide();
+		$("#formNormal").show();
+	}
+}
