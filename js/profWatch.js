5606b236284211c67a18b5bb3539a84d29635b48
Matteo Polsinelli <netrider91@gmail.com>
2016-01-16 17:13:09 +0100

    added some features

--- js/profWatch.js
+++ js/profWatch.js
@@ -1,20 +1,4 @@
 $( document ).ready(function() {
-	console.log("profWathc.js");
-	$("#togglePosition").on("toggle",function(){
-		toggleStatus();
-	});
+	
 
 });
-
-function toggleStatus()
-{
-	if($("#togglePosition").hasClass("active"))
-	{
-		$("#formGps").show();
-		$("#formNormal").hide();
-
-	}else {
-		$("#formGps").hide();
-		$("#formNormal").show();
-	}
-}
