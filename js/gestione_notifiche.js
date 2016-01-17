e5ed7327d95d5eecfc6a200c578703c91d98e2f6
daniel87 <daniel87@daniel87-K53SV>
2016-01-17 22:35:14 +0100

    modifica gestione notifche

--- js/gestione_notifiche.js
+++ js/gestione_notifiche.js
@@ -1,22 +1,57 @@
 $( document ).ready(function() {
+	
+
+	$("#cancella_notifiche").on("click",function(){
+		delete_notifications();
+	});	
+	
+	
+function delete_notifications(){
+	$('#notifica_1').animate({left: "3000px"}, 1000);
+	setTimeout(function(){
+		$('#notifica_2').animate({left: "3000px"}, 800);
+		setTimeout(function(){
+			$('#notifica_3').animate({left: "3000px"}, 800);
+			setTimeout(function(){
+				$('#notifica_4').animate({left: "3000px"}, 800);
+				setTimeout(function(){
+					$('#notifica_5').animate({left: "3000px"}, 800);
+					setTimeout(function(){
+						$('#notifica_6').animate({left: "3000px"}, 800);
+						
+						}, 50);
+					
+					}, 50);
+				
+				}, 50);
+			
+			}, 50);
+		
+		}, 50);
+	}	
 	var list = document.getElementById('slippylist');
 
 
 list.addEventListener('slip:beforeswipe', function(e) {
-    if (shouldNotSwipe(e.target)) {
-        e.preventDefault(); // won't move sideways if prevented
-    }
+   
 });
 
 list.addEventListener('slip:swipe', function(e) {
     // e.target list item swiped
-    if (thatWasSwipeToRemove) {
-        // list will collapse over that element
+   
         e.target.parentNode.removeChild(e.target);
-    } else {
-        e.preventDefault(); // will animate back to original position
-    }
+   
 });
+
+list.addEventListener('slip:beforereorder', function(e) {
+	
+         e.preventDefault();
+     
+});
+
+list.addEventListener('slip:afterswipe', function(e){
+    e.target.parentNode.removeChild(e.target);
+}, false);
 
 new Slip(list);
 })
