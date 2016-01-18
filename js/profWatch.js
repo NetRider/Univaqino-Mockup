00e48062fb23b131f47955d0781d16e5c795d477
Matteo Polsinelli <netrider91@gmail.com>
2016-01-18 12:34:59 +0100

    bug fixing

--- js/profWatch.js
+++ js/profWatch.js
@@ -1,4 +1,13 @@
 $( document ).ready(function() {
-	
+	$("#togglePosition").on("toggle",function(){
+		if($("#togglePosition").hasClass("active"))
+		{
+			$("#formGps").show();
+			$("#formNormal").hide();
+		}else {
+			$("#formGps").hide();
+			$("#formNormal").show();
+		}
+	});
 
 });
