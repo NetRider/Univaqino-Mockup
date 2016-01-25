df17d983d957030bfcc4a058776b9dc80ea48c07
Matteo Polsinelli <netrider91@gmail.com>
2016-01-25 12:31:37 +0100

    no message

--- www/js/profWatch.js
+++ www/js/profWatch.js
@@ -1,8 +1,9 @@
 $( document ).ready(function() {
 
+	console.log("ciao");
+
 	$("#togglePosition").on("toggle",function(){
-		console.log("ciao");
-		
+
 		if($("#togglePosition").hasClass("active"))
 		{
 			$("#formGps").show();
@@ -13,9 +14,12 @@ $( document ).ready(function() {
 		}
 	});
 
-	$("#buttonPosizione").on("click",function() {
-		console.log("cia");
-		$("#selezionaPos").addClass("active");
-	});
+	console.log("ciao due volte");
+
+	$("#segnalaPosButton").on("click",function(){
+		console.log("ciao puluto pippo");
+
+  	});
+
 
 });
